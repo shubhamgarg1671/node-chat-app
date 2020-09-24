f0d5c4fe809bb1468ee2666007f150cab717a606
unknown <shubhamgarg1671@gmail.com>
2020-09-24 20:11:09 +0530

    create generate message utility

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const socketIO = require('socket.io');
 const { Socket } = require('dgram');
 
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const app = express()
 var server = http.createServer(app);
@@ -17,24 +18,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app'
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user Joined',
-        createAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message) => {
         console.log('CreateMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
