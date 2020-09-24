5b779b6a63c3913b527f81640af8ada538d536bc
unknown <shubhamgarg1671@gmail.com>
2020-09-24 19:55:46 +0530

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -17,6 +17,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user Joined',
+        createAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('CreateMessage', message);
         io.emit('newMessage', {
@@ -24,7 +35,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createAt: new Date().getTime()
         });
-    
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // })
     });
 
     socket.on('disconnect', () => {
