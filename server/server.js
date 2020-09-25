00d2efc319902577c29a8226704aeac18f631e10
unknown <shubhamgarg1671@gmail.com>
2020-09-25 09:46:44 +0530

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log('CreateMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('this is from server');
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
